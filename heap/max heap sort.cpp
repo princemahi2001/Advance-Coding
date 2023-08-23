7390b0542ef9fcd4967b0f89d11300ba9fc9ccc8
princemahi2001 <94538166+princemahi2001@users.noreply.github.com>
2023-08-23 15:33:26 +0530

    Update max heap sort.cpp

--- heap/max heap sort.cpp
+++ heap/max heap sort.cpp
@@ -1,5 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
+int n;
+void insert(int a[],int size,int value)
+{
+    a[size]=value;
+    n=size+1;
+}
+
+void deleteelement(int a[],int size,int index)
+{
+    for(int i=index;i<size;i++)
+    {
+        a[i]=a[i+1];
+    }
+    n=size-1;
+}
+
 void maxheap(int a[],int n)
 {
     for(int i=n-1;i>0;i--)
@@ -26,12 +42,21 @@ void maxheap(int a[],int n)
 }
 int main()
 {
-    int n;
+    int val,del;
     cin>>n;
     int a[n];
     for(int i=0;i<n;i++)
     {
         cin>>a[i];
     }
+    maxheap(a,n);
+    cout<<endl<<"enter element you want to insert: ";
+    cin>>val;
+    insert(a,n,val);
+    maxheap(a,n);
+    
+    cout<<endl<<"enter index you want to delete: ";
+    cin>>del;
+    deleteelement(a,n,del);
     maxheap(a,n);
 }
